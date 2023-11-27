22d12eb019305fe51e454e479bfcff0f4803d333
Chamod Isuranga <122660253+chamodIsuranga@users.noreply.github.com>
2023-11-27 12:14:01 +0530

    structure designed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,43 @@ function App() {
       <header className="header">
         Users
       </header>
+      <div className="container">
+        <div className='title'>
+          Users
+        </div>
+        <div className='cards'>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p className='first_name'>first_name</p>
+            <p>email</p>
+          </div>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p>first_name</p>
+            <p>email</p>
+          </div>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p>first_name</p>
+            <p>email</p>
+          </div>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p>first_name</p>
+            <p>email</p>
+          </div>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p>first_name</p>
+            <p>email</p>
+          </div>
+          <div className='card'>
+            <img className='user_image' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' width={"100%"} />
+            <p>first_name</p>
+            <p>email</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
